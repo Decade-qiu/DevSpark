247d99d9722faa2df2eaaf32d3647723b72351bb
Decade-qiu <3237550600@qq.com>
2026-02-10 21:46:09 +0800

    feat: bootstrap backend app

---
+++ backend/src/main/java/com/devspark/DevSparkApplication.java
@@ -0,0 +1,11 @@
+package com.devspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSparkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DevSparkApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/devspark/DevSparkApplicationTests.java
@@ -0,0 +1,11 @@
+package com.devspark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSparkApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
